d3a6ba8b578ef6d3242af53411895cf7aed4195c
wpkai520 <15659265808@163.com>
2024-06-04 05:33:22 +0800

    add generative data file with pde euqations

--- builders/burgers2d_physics.py
+++ builders/burgers2d_physics.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 import os
 import numpy as np
@@ -20,34 +22,97 @@ class Burgers1dBuilder:
         # help path expand to absolute style in different platforms
         data_path = os.path.expanduser(path)
         data_mat = io.loadmat(data_path)
+        # (samples, t, x)
+        u_0_inputs = np.array(data_mat['input'])
         usol = np.array(data_mat['output'])
 
         # some fixed parameters for the number of training examples
         # number of locations for evulating the initial condition
-        P_ics_train = 101
+        self.P_ics_train = 101
         # number of locations for evulating the boundary condition
-        P_bcs_train = 100
+        self.P_bcs_train = 100
         # number of locations for evulating the PDE residual
-        P_res_train = 2500
+        self.P_res_train = 2500
         # resolution of uniform grid for the test data
-        P_test = 101
+        self.P_test = 101
 
         # input samples shape(num_samples, t, x)
         u0_train = usol[:n_train, 0, :]
 
         # generate training data with pde equation
         # Generate training data for inital condition
-        u_ics_train, y_ics_train, s_ics_train = self.generate_one_ics_training_data()
+        u_ics_train, y_ics_train, s_ics_train = self.generate_one_ics_training_data(u0_train)
         # Generate training data for boundary condition
-        u_bcs_train, y_bcs_train, s_bcs_train = self.generate_one_bcs_training_data()
+        u_bcs_train, y_bcs_train, s_bcs_train = self.generate_one_bcs_training_data(u0_train)
         # Generate training data for PDE residual
-        u_res_train, y_res_train, s_res_train = self.generate_one_res_training_data()
+        u_res_train, y_res_train, s_res_train = self.generate_one_res_training_data(u0_train)
 
-    def generate_one_ics_training_data(self):
-        return 0
+        # create the training dataset
 
-    def generate_one_bcs_training_data(self):
-        return 0
+        # pre-process the test data
+        x_test = torch.tensor(u0_train[-n_test:, :], dtype=torch.float)
+        y_test = torch.tensor(usol[-n_test:, :], dtype=torch.float)
 
-    def generate_one_res_training_data(self):
-        return 0
+        # create the test dataset
+
+    def generate_one_ics_training_data(self, u0_train):
+        """
+        generate ics training data corresponding to one input sample
+
+        :return:
+        """
+        t_0 = np.zeros((self.P_ics_train, 1))
+        x_0 = np.linspace(0, 1, self.P_ics_train)[:, None]
+
+        y = np.stack([t_0, x_0])
+        u = np.tile(u0_train, (self.P_ics_train, 1))
+        s = u0_train
+
+        return u, y, s
+
+    def generate_one_bcs_training_data(self, u0_train):
+        """
+        generate bcs training data corresponding to one input sample
+        :param u0_train:
+        :param P_bcs_train:
+        :return:
+        """
+        t_bc = random.uniform((self.P_bcs_train, 1))
+        # u(0, t)
+        x_bc1 = np.zeros((self.P_bcs_train, 1))
+        # u(1, t)
+        x_bc2 = np.zeros((self.P_bcs_train, 1))
+
+        y1 = np.hstack([t_bc, x_bc1])
+        y2 = np.hstack([t_bc, x_bc2])
+
+        u = np.tile(u0_train, (self.P_bcs_train, 1))
+        y = np.hstack([y1, y2])
+        s = np.zeros((self.P_bcs_train, 1))
+
+        return u, y, s
+
+    def generate_one_res_training_data(self, u0_train):
+        """
+        generate residual training data corresponding to one input sample
+        :param u0_train:
+        :param P_res_train:
+        :return:
+        """
+        t_res = random.uniform((self.P_res_train, 1))
+        x_res = random.uniform((self.P_res_train, 1))
+
+        u = np.tile(u0_train, (self.P_res_train, 1))
+        y = np.hstack([t_res, x_res])
+        s = np.zeros((self.P_res_train, 1))
+
+        return u, y, s
+
+
+if __name__ == '__main__':
+    test_builder = Burgers1dBuilder(100,
+                                    100,
+                                    "E:\\dfno\\PIOperator\\data\\burgers\\2d\\burgers_2d.mat"
+                                    )
+
+
